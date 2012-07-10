3efad5768039547559e6c584aece9ff991a0449c
Asher <asher@ridiculouspower.com>
2012-07-10 18:34:21 -0400

    Revert "update for removing module-cluster dependency"
    This reverts commit 91f469ae6e8a8d950d07834d813a5ee8e6285bf9.

--- lib/array/sorted/compositing.rb
+++ lib/array/sorted/compositing.rb
@@ -1,6 +1,8 @@
 
 require 'array/sorted'
 require 'array/compositing'
+#require_relative '../../../../../hooked_objects/array-sorted/lib/array-sorted.rb'
+#require_relative '../../../../array-compositing/lib/array-compositing.rb'
 
 # namespaces that have to be declared ahead of time for proper load order
 require_relative './namespaces'
@@ -9,12 +11,6 @@ require_relative './namespaces'
 require_relative './requires.rb'
 
 class ::Array::Sorted::Compositing < ::Array::Hooked
-
-  alias_method :non_cascading_set, :[]=
-
-  alias_method :non_cascading_insert, :insert
-
-  alias_method :non_cascading_delete_at, :delete_at
 
   include ::Array::Sorted::Compositing::ArrayInterface
 
